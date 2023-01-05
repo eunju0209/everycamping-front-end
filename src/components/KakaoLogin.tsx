688c22b5ddeeea3de3875edb8b071431902a4a5b
재이 <>
2023-01-05 21:15:29 +0900

    카카오로그인

--- src/components/KakaoLogin.tsx
+++ src/components/KakaoLogin.tsx
@@ -1,8 +1,19 @@
 const KakaoLogin = () => {
+  const onClick = () => {
+    window.Kakao.Auth.authorize({
+      redirectUri: "http://localhost:5173/",
+    });
+  };
+
   return (
-    <div className="flex justify-center mt-4 p-1.5 bg-yellow-300">
-      Kakao Login
-    </div>
+    <>
+      <button
+        className="flex justify-center mt-4 p-1.5 bg-yellow-300"
+        onClick={onClick}
+      >
+        Kakao Login
+      </button>
+    </>
   );
 };
 
