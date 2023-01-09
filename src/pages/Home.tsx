c7c50b80663fc630eed99daefcfbf1799afd7bc2
eunju0209 <eunjh33@gmail.com>
2023-01-09 14:47:00 +0900

    home carousel 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,9 +3,9 @@ import ProductList from '../components/ProductList';
 export default function Home() {
   return (
     <>
-      <h1 className='text-3xl font-semibold mb-4 text-center'>NEW</h1>
+      <h1 className='text-3xl font-semibold mb-4 mt-10 text-center'>NEW</h1>
       <ProductList category='all' filter='new' />
-      <h1 className='text-3xl font-semibold mb-4 mt-12 text-center'>BEST</h1>
+      <h1 className='text-3xl font-semibold mb-4 mt-14 text-center'>BEST</h1>
       <ProductList category='all' filter='best' />
     </>
   );
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      maxHeight: {
+        '700px': '700px',
+      },
+    },
   },
   plugins: [require('daisyui')],
   daisyui: {
