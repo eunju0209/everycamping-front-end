361ac7a831a83cf94d09db63d6b111a5b80a211f
재이 <>
2023-01-16 19:22:38 +0900

    비밀번호 수정

--- src/pages/User/PasswordEdit.tsx
+++ src/pages/User/PasswordEdit.tsx
@@ -1,5 +1,12 @@
+import PasswordEditComp from '../../components/UserInfo/PasswordEditComp';
+
 const PasswordEdit = () => {
-  return <div>비밀번호 수정</div>;
+  return (
+    <div className='flex flex-col justify-center mx-auto w-80'>
+      <p className='flex justify-center text-4xl'>비밀번호 수정</p>
+      <PasswordEditComp />
+    </div>
+  );
 };
 
 export default PasswordEdit;
