fdaaf0271bd4bddd5d0e1e60d44574019b58d8e1
재이 <>
2023-01-23 02:02:54 +0900

    stompJs 테스트 중.

--- src/api/chat.ts
+++ src/api/chat.ts
@@ -1,35 +1,46 @@
+import { Stomp } from '@stomp/stompjs';
 import axios from 'axios';
 import SockJs from 'sockjs-client'
 import StompJs from 'stompjs'
+import { getCookie } from '../store/cookie';
 import { authAxios } from './authAxios';
 
 const sock = new SockJs('/api/websocket');
 const stomp = StompJs.over(sock);
+// const stomp = Stomp.over(sock);
+// const stomp = StompJs.over(new WebSocket('ws://ec2-43-200-2-48.ap-northeast-2.compute.amazonaws.com/websocket')
+      //  );
+
+
 
 export const getRoomId = async () => {
-    const result = await axios.post('/api/questions');
-    return result.data
+  const result = await axios.post('/api/questions');
+  return result.data
 }
 
-export const stompConnect = (roomId : string, setMessages : React.Dispatch<React.SetStateAction<string[]>>) => {
-  try {
-        stomp.connect(
-            {}
-            // token
-            , (frame) => {
-                console.log('Connected : ' + frame);
-                stompSubscribe(roomId, setMessages);
-            }
-        );
-  } catch (err) {
-    console.error(err)
-    }
+export const stompConnect = (roomId: string, setMessages: React.Dispatch<React.SetStateAction<string[]>>) => {
+  console.log(stomp)
+  stomp.connect({
+    //  login: '',
+    // passcode: '',
+   }
+      // getCookie('accessToken')
+      , (frame) => {
+        
+        console.log('Connected : ' + frame);
+        stompSubscribe(roomId, setMessages);
+      }, 
+      (error: any) => {
+        console.error(error)
+        console.log('a')
+      }
+  );
 };
 
 const stompSubscribe = (roomId : string, setMessages : React.Dispatch<React.SetStateAction<string[]>>) => {
     stomp.subscribe(
         `/topic/questions/${roomId}`,
-        (data) => {
+        (data: { body: string; }) => {
             const newMessage = JSON.parse(data.body).content;
             setMessages(prev =>  [...prev, newMessage] );
             console.log('subscribe : ', data);
@@ -52,7 +63,7 @@ export const sendMessage = (roomId : string, message : string) => {
     };
     stomp.send(`/questions/${roomId}`,
         {
-            Authorization: "this is token"
+            Authorization: "accessToken"
         }
         ,JSON.stringify(data));
 };
