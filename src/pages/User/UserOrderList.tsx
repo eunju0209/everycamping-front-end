a265c1803df77442e1a384979c186a5594f4fc55
재이 <>
2023-01-23 21:43:51 +0900

    seller/user orderList 변경

--- src/pages/User/UserOrderList.tsx
+++ src/pages/User/UserOrderList.tsx
@@ -2,34 +2,16 @@ import { useEffect, useState } from 'react';
 import { getUserOrderLIst } from '../../api/orderService';
 import UserOrderCard from '../../components/OrderList/UserOrderCard';
 
-// String address //배송지 주소
-// String phone //수령자 연락처
-// Long productId //상품id
-// String productNameSnapshot // 주문 시 상품명
-// Integer stockPriceSnapshot // 주문 시 개당 가격
-// String imageUriSnapshot // 주문 시 상품 이미지 url
-// Long orderProductId //주문상품id (주문확정, 주문취소 시 필요)
-// Integer quantity //주문수량
-// Integer amount // 총 금액
-// OrderStatus status //주문 상태 (COMPLETE|CONFIRM|CANCEL|SETTLEMENT)
-// LocalDateTime createdAt : 주문 일자
-
-export type UserOrderListType = {
-  productId: number;
-  orderProductId: number;
-  imageUriSnapshot: string;
-  productNameSnapshot: string;
-  quantity: number;
-  amount: number;
-  userName: string;
-  phone: string;
-  address: string;
-  status: string;
+export type UserOrderType = {
+  id: number;
+  representProductName: string;
+  orderProductCount: number;
+  totalAmount: number;
   createdAt: string;
 };
 
 export default function UserOrderList() {
-  const [orderList, setOrderList] = useState([]);
+  const [orderList, setOrderList] = useState<UserOrderType[]>([]);
 
   useEffect(() => {
     (async () => {
@@ -45,11 +27,11 @@ export default function UserOrderList() {
 
   return (
     <div>
-      <div className='flex flex-col justify-center mx-auto max-w-cartDiv'>
+      <div className='flex flex-col justify-center mx-auto max-w-orderList'>
         <h1 className='flex justify-center text-4xl'>주문내역</h1>
         <div className='mt-7'>
-          {orderList.map((list: UserOrderListType) => {
-            return <UserOrderCard key={list.productId} list={list} />;
+          {orderList.map((list: UserOrderType) => {
+            return <UserOrderCard key={list.id} list={list} />;
           })}
         </div>
       </div>
