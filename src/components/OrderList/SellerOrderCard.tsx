094e014dcdab00aa9e72ec58523d77ab385fa2ca
재이 <>
2023-01-23 21:43:08 +0900

    seller / user order List 방식 변경

--- src/components/OrderList/SellerOrderCard.tsx
+++ src/components/OrderList/SellerOrderCard.tsx
@@ -1,5 +1,6 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { SellerOrderListType } from '../../pages/SellerOrderList';
+import SellerOrderDetail from './SellerOrderDetailCard';
 
 type SellerOrderListCardProps = {
   list: SellerOrderListType;
@@ -7,22 +8,17 @@ type SellerOrderListCardProps = {
 
 const SellerOrderListCard = ({
   list: {
-    productId,
-    customerId,
     orderProductId,
-    imageUriSnapshot,
+    productId,
     productNameSnapshot,
     quantity,
     amount,
-    userName,
-    phone,
-    address,
-    status,
     createdAt,
+    status,
   },
 }: SellerOrderListCardProps) => {
   const [orderState, setOrderState] = useState('');
-  const labelRef = useRef<HTMLUListElement>(null);
+  const [popDetail, setPopDetail] = useState(false);
 
   useEffect(() => {
     switchStatus();
@@ -31,7 +27,7 @@ const SellerOrderListCard = ({
   const switchStatus = () => {
     switch (status) {
       case 'COMPLETE':
-        setOrderState('주문 완료');
+        setOrderState('결제 완료');
         break;
       case 'CONFIRM':
         setOrderState('구매 확정');
@@ -40,72 +36,44 @@ const SellerOrderListCard = ({
         setOrderState('주문 취소');
         break;
       case 'SETTLEMENT':
-        setOrderState('주문 완료');
+        setOrderState('결제 완료');
         break;
     }
   };
+  const popUpDetail = () => {
+    setPopDetail(true);
+  };
   return (
     <div className='flex rounded bg-white mt-3 p-2'>
-      <div className='flex align-center h-40 p-2'>
+      {/* <div className='flex align-center h-40 p-2'>
         <img src={imageUriSnapshot} className='rounded' />
-      </div>
-      <div className='flex justify-between w-full p-2'>
+      </div> */}
+      <div
+        className='flex justify-between w-full p-2 cursor-pointer'
+        onClick={popUpDetail}
+      >
         <div className='flex flex-col justify-between w-full p-1'>
           <ul>
             <li className='text-xl'>제품명 : {productNameSnapshot}</li>
             <ul className='text-sm'>
               <li>수량 : {quantity}</li>
+              <li>결제금액 : {amount.toLocaleString()}원</li>
               <li>주문날짜 : {createdAt}</li>
             </ul>
           </ul>
-          <div className='w-full'>
-            <ul className='flex justify-between'>
-              <li>주문자명 : {userName}</li>
-              <li>연락처 : {phone}</li>
-              <li>결제금액 : {amount.toLocaleString()}원</li>
-            </ul>
-            <ul className='flex justify-between'>
-              <li>주소 : {address}</li>
-            </ul>
-          </div>
         </div>
-        <div>
+        <div className='flex flex-col justify-center'>
           <div className='flex justify-center btn-primary w-24 rounded-lg my-1 p-2'>
             {orderState}
           </div>
         </div>
-        {/* <div className='dropdown'>
-          <label tabIndex={0} className='btn m-1 min-w-100px'>
-            {orderState}
-          </label>
-          <ul
-            tabIndex={0}
-            className='dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52'
-            ref={labelRef}
-          >
-            <li>
-              <p
-                onClick={() => {
-                  setOrderState('주문 확인');
-                  labelRef.current ? labelRef.current.blur() : '';
-                }}
-              >
-                주문 확인
-              </p>
-            </li>
-            <li>
-              <p
-                onClick={() => {
-                  setOrderState('배송 준비중');
-                  labelRef.current ? labelRef.current.blur() : '';
-                }}
-              >
-                배송 준비중
-              </p>
-            </li>
-          </ul>
-        </div> */}
       </div>
+      <SellerOrderDetail
+        id={orderProductId}
+        popDetail={popDetail}
+        setPopDetail={setPopDetail}
+        orderState={orderState}
+      />
     </div>
   );
 };
--- src/components/OrderList/UserOrderDetailCard.tsx
+++ src/components/OrderList/UserOrderDetailCard.tsx
@@ -1,69 +1,115 @@
-import { Dispatch } from 'react';
-import { UserOrderListType } from '../../pages/User/UserOrderList';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { patchOrderCancel, patchOrderConfirm } from '../../api/orderService';
+import { OrderItemsType } from './UserOrderDetail';
 
 type UserOrderDetailCardProps = {
-  popDetail: boolean;
-  setPopDetail: Dispatch<React.SetStateAction<boolean>>;
-  itemStatus: string;
-  orderDetail: UserOrderListType;
+  itemsDetail: OrderItemsType;
 };
 
 const UserOrderDetailCard = ({
-  popDetail,
-  setPopDetail,
-  itemStatus,
-  orderDetail: {
+  itemsDetail: {
+    id,
     productId,
-    orderProductId,
+    stockPriceSnapshot,
     imageUriSnapshot,
     productNameSnapshot,
     quantity,
     amount,
-    userName,
-    phone,
-    address,
     status,
-    createdAt,
+    sellerId,
+    sellerNickName,
+    sellerEmail,
+    sellerPhone,
   },
 }: UserOrderDetailCardProps) => {
-  const onClick = () => {
-    setPopDetail(false);
+  const navigate = useNavigate();
+  const [itemStatus, setItemStatus] = useState('');
+  useEffect(() => {
+    switchStatus();
+  }, []);
+
+  const orderConfirm = async () => {
+    try {
+      if (status !== 'COMPLETE') {
+        return alert('구매 확정 대상이 아닙니다.');
+      }
+      await patchOrderConfirm(productId).then(() =>
+        alert('구매 확정이 완료 되었습니다.')
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const orderCancel = async () => {
+    try {
+      if (status === 'CANCEL') {
+        return alert('취소 대상이 아닙니다.');
+      }
+      await patchOrderCancel(productId).then(() =>
+        alert('주문이 취소 되었습니다.')
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const switchStatus = () => {
+    switch (status) {
+      case 'COMPLETE':
+        setItemStatus('주문 완료');
+        break;
+      case 'CONFIRM':
+        setItemStatus('구매 확정');
+        break;
+      case 'CANCEL':
+        setItemStatus('주문 취소');
+        break;
+      case 'SETTLEMENT':
+        setItemStatus('구매 확정');
+        break;
+    }
   };
   return (
-    <div
-      className={`modal ${
-        popDetail ? 'visible opacity-100 pointer-events-auto' : ''
-      } `}
-    >
-      <div className='modal-box'>
-        <h3 className='flex justify-center font-bold text-lg'>
-          주문 상세 정보
-        </h3>
-        <div className='flex rounded bg-white mt-3 p-2'>
-          <div className='flex align-center h-40 p-2'>
-            <img src={imageUriSnapshot} className='rounded' />
-          </div>
-          <div className='flex justify-between w-full p-2'>
-            <div className='flex flex-col justify-between p-1 cursor-pointer'>
-              <ul>
-                <li className='text-xl'>제품명 : {productNameSnapshot}</li>
-                <ul className='text-sm'>
-                  <li>수량 : {quantity}</li>
-                  <li>결제금액 : {amount.toLocaleString()}원</li>
-                </ul>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div>
+    <div>
+      <div className='flex align-center h-40 p-2'>
+        <img src={imageUriSnapshot} className='rounded' />
+      </div>
+      <div className='flex justify-between w-full p-2'>
+        <div className='flex flex-col justify-between p-1 cursor-pointer'>
           <ul>
-            <li>주문날짜 : {createdAt}</li>
-            <li>진행상황 : {itemStatus}</li>
+            <li className='text-xl'>제품명 : {productNameSnapshot}</li>
+            <ul className='text-sm'>
+              <li>수량 : {quantity}</li>
+              <li>결제금액 : {amount.toLocaleString()}원</li>
+              <li>진행상황 : {itemStatus}</li>
+            </ul>
           </ul>
         </div>
-        <div className='modal-action'>
-          <button className='btn pointer-events-auto' onClick={onClick}>
-            확인
+        <div className='flex flex-col justify-between p-1 cursor-pointer'>
+          <ul className='text-sm'>
+            <li>판매자 : {sellerNickName}</li>
+            <li>판매자 연락처 : {sellerPhone}</li>
+          </ul>
+        </div>
+        <div className='flex flex-col justify-center'>
+          <button
+            className='btn btn-primary btn-sm my-1'
+            onClick={orderConfirm}
+          >
+            구매확정
+          </button>
+          <button
+            className='btn btn-primary btn-sm my-1'
+            onClick={() =>
+              navigate('/review/new', {
+                state: productId,
+              })
+            }
+          >
+            리뷰작성
+          </button>
+          <button className='btn btn-primary btn-sm my-1' onClick={orderCancel}>
+            {status !== 'COMPLETE' ? '환불요청' : '주문취소'}
           </button>
         </div>
       </div>
