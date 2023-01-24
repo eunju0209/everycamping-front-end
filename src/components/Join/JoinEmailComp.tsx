2d669170a6dac81ebe38d2a135f8d76a955e50b4
재이 <>
2023-01-25 01:52:40 +0900

    이메일 유효성 검사

--- src/components/Join/JoinEmailComp.tsx
+++ src/components/Join/JoinEmailComp.tsx
@@ -57,17 +57,20 @@ const JoinEmailComp = () => {
     if (joinInfo.password.length < 6 || joinInfo.password !== passwordConfirm)
       return;
 
-    // if (code === '') {
-    //   return alert('이메일 인증이 완료되지 않았습니다.');
-    // }
+    if (code === '') {
+      return alert('이메일 인증이 완료되지 않았습니다.');
+    }
     try {
       if (isSeller) {
         await postSellerJoin(joinInfo).then(() => navigate('/login'));
       } else {
         await postUserJoin(joinInfo).then(() => navigate('/login'));
       }
-    } catch (error) {
+    } catch (error: any) {
       console.error(error);
+      if (error.response.data.message === '사용중인 이메일입니다.') {
+        return alert('사용중인 이메일 입니다.');
+      }
       alert('회원 가입에 실패 했습니다.');
     }
   };
@@ -134,7 +137,7 @@ const JoinEmailComp = () => {
             인증하기
           </button>
         </div>
-        {/* <div className='flex relative mt-2 w-full hidden' ref={codeDivRef}>
+        <div className='flex relative mt-2 w-full hidden' ref={codeDivRef}>
           <input
             className='p-2 input w-full max-w-xs bg-white focus:outline-none'
             name='code'
@@ -152,7 +155,7 @@ const JoinEmailComp = () => {
           >
             인증완료
           </button>
-        </div> */}
+        </div>
         <div className='flex relative mt-2 w-full'>
           <input
             className='p-2 input w-full max-w-xs bg-white focus:outline-none'
