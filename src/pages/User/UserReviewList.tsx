0b30ce4faa986c52feb7da221758dcf0d3208d04
eunju0209 <eunjh33@gmail.com>
2023-01-25 22:45:56 +0900

    review api 수정

--- src/pages/User/UserReviewList.tsx
+++ src/pages/User/UserReviewList.tsx
@@ -1,9 +1,18 @@
+import { useEffect, useState } from 'react';
 import ReviewList from '../../components/Review/ReviewList';
+import { useUserInfo } from '../../store/UserInfoProvider';
 
 export default function UserReviewList() {
+  const { userInfo } = useUserInfo();
+  const [customerId, setCustomerId] = useState(0);
+
+  useEffect(() => {
+    userInfo.customerId ? setCustomerId(userInfo.customerId) : setCustomerId(0);
+  }, [userInfo]);
+
   return (
     <>
-      <ReviewList customerId={'2'} />
+      <ReviewList customerId={customerId} />
     </>
   );
 }
