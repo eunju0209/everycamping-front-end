b8bc56bdc241c79e0938c334caecaa9eb735527f
재이 <>
2023-01-27 02:14:15 +0900

    에러페이지

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,3 +1,19 @@
+import { useNavigate } from 'react-router-dom';
+
 export default function NotFound() {
-  return <div>NotFound</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className='hero min-h-screen bg-base-200'>
+      <div className='hero-content text-center'>
+        <div className='max-w-md'>
+          <h1 className='text-5xl font-bold'>404 NOT FOUND</h1>
+          <p className='py-6'>요청하신 페이지를 찾을 수 없습니다.</p>
+          <button className='btn btn-error' onClick={() => navigate('/')}>
+            Home
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
