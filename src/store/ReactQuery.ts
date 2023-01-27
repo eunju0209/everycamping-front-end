c8e8ba51e1988fff020f13695e41b1fe97dcf2a7
재이 <>
2023-01-27 23:34:31 +0900

    판매자 컨펌 react query 사용

--- src/store/ReactQuery.ts
+++ src/store/ReactQuery.ts
@@ -1,7 +1,6 @@
-import { useQuery, UseQueryOptions } from '@tanstack/react-query';
-import { AxiosResponse } from 'axios';
+import { useQuery } from '@tanstack/react-query';
+import { getAdminSellerList } from '../api/adminService';
 import { getCartItems } from '../api/cartService';
-import { getSellerInfo, getUserInfo } from '../api/userService';
 
 export const queryGetCartItems = () => {
   const result = useQuery(['@CartItem'], () => getCartItems(), {
@@ -10,3 +9,11 @@ export const queryGetCartItems = () => {
   });
   return result
 };
+
+export const queryGetAdminSellerList = () => {
+  const result = useQuery(['@AdminSellerList'], () => getAdminSellerList(), {
+    staleTime: Infinity,
+    retry: 1
+  });
+  return result.data
+}
