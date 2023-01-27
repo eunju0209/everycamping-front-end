4c396cce977eab5583cbbc8b06de6def34e7a9ea
eunju0209 <eunjh33@gmail.com>
2023-01-27 14:26:04 +0900

    mypage link 수정

---
+++ src/components/MypageLink/AdminLink.tsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+export default function AdminLink() {
+  return (
+    <>
+      <li>
+        <Link to='/sellerConfirm'>판매자승인</Link>
+      </li>
+    </>
+  );
+}
---
+++ src/components/MypageLink/SellerLink.tsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+
+export default function SellerLink() {
+  return (
+    <>
+      <li>
+        <Link to='/mypage/seller/orders'>주문내역</Link>
+      </li>
+      <li>
+        <Link to='/products/new'>제품등록</Link>
+      </li>
+      <li>
+        <Link to='/mypage/products'>등록제품목록</Link>
+      </li>
+      <li>
+        <Link to='/userInfo'>회원정보</Link>
+      </li>
+    </>
+  );
+}
