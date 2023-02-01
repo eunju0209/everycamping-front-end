ae2e3f6b715ccff05576793dae13f44013c497d9
재이 <>
2023-02-01 19:13:12 +0900

    소셜로그인 수정

--- src/components/Login/socialLogin/KakaoLoginCallback.tsx
+++ src/components/Login/socialLogin/KakaoLoginCallback.tsx
@@ -1,49 +1,34 @@
 import axios from 'axios';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { getUserInfo, postUserSocialLogin } from '../../../api/userService';
+import { getUserInfo } from '../../../api/userService';
+import { setCookie } from '../../../store/cookie';
 import { useUserInfo } from '../../../store/UserInfoProvider';
 
 const KaKaoLoginCallback = () => {
   const navigate = useNavigate();
   const { setUserInfo } = useUserInfo();
-  let kakaoRequest: {
-    kakao_account: {
-      email: string;
-      profile: {
-        nickname: string;
-      };
-    };
-  };
 
   useEffect(() => {
     (async () => {
-      try {
-        const AUTHORIZE_CODE = new URL(
-          document.location.toString()
-        ).searchParams.get('code');
-        const REST_API_KEY = import.meta.env.VITE_KAKAO_LOGIN_RESTAPI_KEY;
-        const REDIRECT_URI = 'http://localhost:5173/kakaoLoginCallback';
+      const AUTHORIZE_CODE = new URL(
+        document.location.toString()
+      ).searchParams.get('code');
 
-        const token = await axios.post(
-          `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&code=${AUTHORIZE_CODE}`,
-          {
-            headers: {
-              'Content-Type': 'application/x-www-form-urlencoded',
-            },
-          }
-        );
-        window.Kakao.Auth.setAccessToken(token.data.access_token);
-        kakaoRequest = await window.Kakao.API.request({
-          url: '/v2/user/me',
-        });
+      await axios
+        .get(`api/customers/signin/social/kakao?code=${AUTHORIZE_CODE}`)
+        .then(async (result) => {
+          setCookie('accessToken', result.data.accessToken, {
+            path: '/',
+          });
+          setCookie('refreshToken', result.data.refreshToken, {
+            path: '/',
+          });
+          setCookie('LoginType', 'user', {
+            path: '/',
+          });
 
-        await postUserSocialLogin(
-          kakaoRequest.kakao_account.email,
-          kakaoRequest.kakao_account.profile.nickname
-        ).then(async () => {
           const data = await getUserInfo();
-
           setUserInfo({
             email: data.email,
             nickName: data.nickName,
@@ -51,14 +36,9 @@ const KaKaoLoginCallback = () => {
             customerId: data.id,
             type: 'user',
           });
-
-          navigate('/');
         });
-      } catch (err) {
-        console.log(err);
-        alert('로그인을 실패 했습니다.');
-      }
     })();
+    navigate('/');
   }, []);
   return <div>...loading</div>;
 };
