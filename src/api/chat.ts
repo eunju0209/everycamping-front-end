d8452f0eb542d7c3e7a5813c21482665efe440e5
재이 <>
2023-02-01 19:07:25 +0900

    채팅구현

--- src/api/chat.ts
+++ src/api/chat.ts
@@ -3,30 +3,39 @@ import axios from 'axios';
 import SockJs from 'sockjs-client'
 import { messageDataType } from '../components/Chatting/Chatting';
 import { getCookie } from '../store/cookie';
+import { authAxios } from './authAxios';
 
 const stomp = Stomp.over(() => new SockJs('/api/websocket'));
 
-export const getRoomId = async () => {
-  const result = await axios.post('/api/questions');
+
+export const getRoomId = async (
+  requesterEmail: string,
+  requesterUserType : 'CUSTOMER' | 'SELLER' | 'ADMIN',
+  requesteeEmail : string,
+  requesteeUserType : 'CUSTOMER' | 'SELLER' | 'ADMIN'
+) => {
+  const result = await axios.post('/api/chat-rooms', {
+    requesterEmail,
+    requesterUserType,
+    requesteeEmail,
+    requesteeUserType,
+  });
   return result.data
 }
 
 export const stompConnect = (roomId: string, setMessages: React.Dispatch<React.SetStateAction<messageDataType[]>>) => {
 
   try {
-    // stomp.debug = () => {}
+    stomp.debug = () => {}
     stomp.connect(
     {
       Authorization : getCookie('accessToken')
     }
-    , (frame:string) => {
-      
-      console.log('Connected : ' + frame);
+    , () => {
       stompSubscribe(roomId, setMessages);
     }, 
     (error: any) => {
       console.error(error)
-      console.log('a')
     }
     );
   } catch (error) {
@@ -35,25 +44,23 @@ export const stompConnect = (roomId: string, setMessages: React.Dispatch<React.S
 };
 
 const stompSubscribe = (roomId: string, setMessages: React.Dispatch<React.SetStateAction<messageDataType[]>>) => {
-  // stomp.debug = () => {}
+  stomp.debug = () => {}
     stomp.subscribe(
-        `/topic/questions/${roomId}`,
+        `/topic/chat-rooms/${roomId}`,
         (data : {body : string}) => {
           const newMessage = JSON.parse(data.body);
-          console.log(newMessage)
             setMessages(prev =>  [...prev, newMessage] );
-            console.log('subscribe : ', data);
         },
     );
 }
 
 export const sendMessage = (roomId: string, message: string, userType:string) => {
-  // stomp.debug = () => {}
+  stomp.debug = () => {}
   const data = {
     content: message,
     userType: userType
   };
-  stomp.send(`/questions/${roomId}`,
+  stomp.send(`/chat-rooms/${roomId}`,
   {
     Authorization: getCookie('accessToken')
   },
@@ -66,3 +73,17 @@ export const stompDisConnect = () => {
     console.log("채팅이 종료 되었습니다.");
     });
 };
+
+export const getChatList = async (userEmail : string ,userType : 'CUSTOMER' | 'SELLER' | 'ADMIN') => {
+  const result = await authAxios.get(`/api/chat-rooms?userEmail=${userEmail}&userType=${userType}`)
+  return result.data
+}
+
+export const getChatMessageList = async (chatRoomId:number) => {
+  const result = await authAxios.get(`/api/chat-rooms/${chatRoomId}/messages`);
+  return result.data
+}
+export const deleteChatList = async (chatRoomId:number) => {
+  const result = await authAxios.delete(`/api/chat-rooms/${chatRoomId}`);
+  return result.data
+}
