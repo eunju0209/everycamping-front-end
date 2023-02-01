a8c5783cb59292e6a7887bbe5252504a969483a8
eunju0209 <eunjh33@gmail.com>
2023-02-01 20:29:22 +0900

    netlify route 에러 수정

--- src/api/productsService.ts
+++ src/api/productsService.ts
@@ -89,7 +89,7 @@ async function getItems(
   filter?: string
 ): Promise<ProductType[]> {
   const res = await axios.get(
-    `${PROXY}/products${
+    `/api/products${
       category ? `?category=${category}` : `?size=4&sort=${filter},desc`
     }`
   );
