a2441d1b94791b5ca3d873d037d118f992d07184
재이 <>
2023-02-03 21:30:42 +0900

    캐러셀 이미지 변수 선언 위치 변경. -> 실행 속도 향상

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -2,6 +2,17 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { Carousel } from 'react-responsive-carousel';
 
 export default function Slider() {
+  const banners = [
+    {
+      imagePath:
+        'https://images.unsplash.com/photo-1599403302125-430c37091864?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80',
+    },
+    {
+      imagePath:
+        'https://images.unsplash.com/photo-1542338106-1b4bfe84d5df?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+    },
+  ];
+
   return (
     <Carousel
       showThumbs={false}
@@ -19,14 +30,3 @@ export default function Slider() {
     </Carousel>
   );
 }
-
-const banners = [
-  {
-    imagePath:
-      'https://images.unsplash.com/photo-1599403302125-430c37091864?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80',
-  },
-  {
-    imagePath:
-      'https://images.unsplash.com/photo-1542338106-1b4bfe84d5df?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-  },
-];
